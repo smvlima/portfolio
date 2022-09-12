52eeb9eb7377cbb34c419716d6708506fc657a3f
Sílvia Lima <smvlima26@gmail.com>
2022-09-12 21:32:02 +0100

    aula 1209

--- silvia-lima/my-app/src/pages/news/news.js
+++ silvia-lima/my-app/src/pages/news/news.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./news.scss";
-
 function News() {
+   const [data, setData] = useState([]);
+
+   useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(result => {
+        setData(result);
+      })
+   }, [])
+
   return (
     <div className="news">
       <h1 className="news__headline">Welcome to news</h1>
-      <p className="news__loading">Loading data... </p>
-      <div className="news__list">
-        <article className="news__article">
+      {!data.length? (
+        <p className="news__loading">Loading data... </p>
+      ): (
+        <div className="news__list">
+        {data.map((item)=> (
+          <article key={item.id} className="news__article">
           <a className="news__link" href="#nowhere">
-            <h2 className="news__tile">Título</h2>
-            <p className="news__description">Descrição</p>
+            <h2 className="news__tile">{item.title}</h2>
+            <p className="news__description">{item.body}</p>
           </a>
         </article>
+        ))}
+        
       </div>
+      )}
     </div>
   );
 }
