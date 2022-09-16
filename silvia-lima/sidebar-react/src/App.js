72fb52d36c5bb9d64730f43c2b692793020c0667
Sílvia Lima <smvlima26@gmail.com>
2022-09-16 19:16:30 +0100

    add sidebar

---
+++ silvia-lima/sidebar-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Slidebar from './pages/slidebar/slidebar'
+
+function App() {
+  return (
+    <div className="App">
+      <Slidebar />
+    </div>
+  );
+}
+
+export default App;
