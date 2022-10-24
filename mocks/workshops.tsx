0441ed125d93ccde2e65ceae90e5b8dd4adf9beb
Sílvia Lima <101286293+smvlima@users.noreply.github.com>
2022-10-24 22:48:54 +0100

    Add files via upload

---
+++ mocks/workshops.tsx
@@ -0,0 +1,29 @@
+import { Skills } from "../interfaces/badges";
+
+export const WORKSHOPS: Skills[] = [
+  {
+    id: 1,
+    name: "scrum",
+    title: "Scrum",
+    image:
+      "https://images.credly.com/size/680x680/images/cefdd96e-0583-4024-8258-ae83d1404a84/FL0136.png",
+    link: "https://www.credly.com/badges/94590b04-4136-484d-889d-514de14701aa/public_url",
+  },
+  {
+    id: 2,
+    name: "sass",
+    title: "SASS",
+    image:
+      "https://images.credly.com/size/680x680/images/a7fa3585-3483-4346-bad8-d4919ac932ff/FL0138.png",
+    link: "https://www.credly.com/badges/510c828b-138c-4c6c-bb47-69b7c057f2ea/public_url",
+  },
+  {
+    id: 3,
+    name: "git",
+    title: "GIT",
+    image:
+      "https://images.credly.com/size/680x680/images/37e0dc76-ab72-4f24-befe-2b26f58f5b2d/FL0131.png",
+    link: "https://www.credly.com/badges/60af28b1-2e35-4c8f-8b6c-180a12e9d590/public_url",
+  },
+  
+]
---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/Global.scss";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+
+function MyApp(
+  { Component, pageProps }: AppProps,
+) {
+
+
+  return (
+     <RecoilRoot>   
+          <Component {...pageProps} />
+      </RecoilRoot>
+  );
+}
+
+export default MyApp;
