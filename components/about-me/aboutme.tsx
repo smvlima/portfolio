06282e21a33db2b7d95581ad41650db366f3e976
Sílvia Lima <101286293+smvlima@users.noreply.github.com>
2023-03-07 19:22:05 +0000

    update about text

--- components/about-me/aboutme.tsx
+++ components/about-me/aboutme.tsx
@@ -11,27 +11,22 @@ const Aboutme = () => {
             Full-Stack Web Developer.
           </p>
           <p>
-            I&#39;m from the small village of Alpendorada in the Oporto district. In
-            the beginning of my adult life I lived for 5 years in the City of
-            Oporto, and then at the age of 22 I decided to move to Faro. Here I
-            feel I can have the best of both worlds, the country and the city,
-            plus the pleasure of living near the sea.{" "}
+            I am 27 years old and I am from the small village of Alpendorada in
+            the Oporto district. In the beginning of my adult life I lived for 5
+            years in the City of Oporto, and then at the age of 22 I decided to
+            move to Faro. Here I feel I can have the best of both worlds, the
+            country and the city, plus the pleasure of living near the sea. But,
+            as a lover of challenges, I am not afraid of seizing new
+            opportunities that life offers me.{" "}
           </p>
           <p>
             I realized I was interested in multimedia when I started to study
-            marketing, but I understood my enthusiasm was not about marketing
-            strategies but about producing content. Then I enrolled for a course
-            in multimedia products development in ISMAI. I&#39;ve studied several
-            areas on that course, as illustration, photography, 3D modeling,
-            design, sound, video, gaming, data bases and programming. I was
-            specially interested in programming. Due to this experience, I&#39;ve
-            finally figured out that was my motivation and my goal in this big
-            world of multimedia.
+            marketing, and felt my enthusiasm was not about marketing strategies but about producing content. So, I enrolled for a course of development of multimedia products in ISMAI. here, I&#39;ve studied several areas, as illustration, photography, 3D modeling, design, sound, video, gaming, data bases and programming. I was specially interested in the last one. Due to this experience, I've finally figured out programming was my passion and what I wished for my professional career.
           </p>
           <p>
             After some years invested in self-taught and my personal projects,
-            I&#39;ve decided to start the course of Full-Stack Web Developer which
-            I&#39;m currently studying at FLAG.{" "}
+            I&#39;ve decided to start the course of Full-Stack Web Developer
+            which I&#39;m currently studying at FLAG.{" "}
           </p>
         </div>
         <div className={styles.image}></div>
